a616ee42c56b3944bfb455d69d9536d5110107be
jaden <ljaden.9@gmail.com>
2022-07-23 19:18:17 -0400

    added buttons and functionality

--- script.js
+++ script.js
@@ -5,29 +5,71 @@ function getCpuChoice() {
   return cpuChoice
 }
 
-function getUserChoice() {
-  let choice
+function playRound(playerSelection){
+  // cpu choice
+  const cpuSelection = getCpuChoice()
 
-  while(!(choice === 'R' ||choice === 'P' ||choice === 'S')) {
-    choice = prompt("Pick One: [R]ock // [P]aper [S]cissor").toUpperCase()
+  // check who won
+  const winner = whoWon(playerSelection,cpuSelection)
+  // keep score
+  let cScore = Number(document.getElementById('cpuScore').textContent)
+  let pScore = Number(document.getElementById('playerScore').textContent)
+  if (winner.includes('Won')) {
+    pScore++
+  } else if (winner.includes('Lost')) {
+    cScore++
   }
+
+  // display results
+  const result = document.getElementById('result')
+  result.textContent = `${winner}`
   
-  switch (choice){
-    case 'R':
-      return 'Rock'
-    case 'P':
-      return 'Paper'
-    case 'S':
-      return 'Scissor'  
-  }
+  // update score
+  let playerScore = document.getElementById('playerScore')
+  let cpuScore = document.getElementById('cpuScore')
+
+  playerScore.textContent = pScore
+  cpuScore.textContent = cScore
+
+  if(pScore === 5 || cScore === 5) {
+    result.textContent = pScore===5? 'YOU WON!':'YOU LOST!'
+    playerScore.textContent = 0
+    cpuScore.textContent = 0
+  } 
+
 }
 
-function playRound(){
-  let playerSelection = getUserChoice(),cpuSelection =getCpuChoice()
+// function game(){
+//   let score = {"YOU":0,"CPU":0}
 
+//   for(let i=0; i<5;i++){
+//     let game = playRound()
+//     console.log(game)
+
+//     if(game.includes('Won')) {
+//       score['YOU']++
+//     } else if(game.includes('Lost')) {
+//       score['CPU']++
+//     }
+//     console.log('SCORE: ',score)
+//   }
+//   return score['YOU']>=3? "YOU ARE THE WINNER": score['CPU']>=3? "YOU LOST": "IT'S A TIE "
+// }
+
+// const choice = document.querySelectorAll('.choice')
+// choice.forEach(btn => {
+//   btn.addEventListener('click', (e) => {
+//     let selection = btn.innerHTML
+//     playRound(selection)
+//     // console.log(`${btn.innerHTML}`)
+//   })
+// })
+
+function whoWon(playerSelection, cpuSelection) {
   let wonStatement = `You Won, ${playerSelection} beats ${cpuSelection}`
   let lostStatement = `You Lost, ${cpuSelection} beats ${playerSelection}`
   let drawStatement = "It's a Draw"
+
   // Player pick 'Rock'
   if(playerSelection === 'Rock'){
     switch (cpuSelection){
@@ -36,6 +78,7 @@ function playRound(){
       case 'Paper':
         return lostStatement
       case 'Scissor':
+
         return wonStatement
     }
   }
@@ -63,19 +106,9 @@ function playRound(){
   }
 }
 
-function game(){
-  let score = {"YOU":0,"CPU":0}
-
-  for(let i=0; i<5;i++){
-    let game = playRound()
-    console.log(game)
-
-    if(game.includes('Won')) {
-      score['YOU']++
-    } else if(game.includes('Lost')) {
-      score['CPU']++
-    }
-    console.log('SCORE: ',score)
-  }
-  return score['YOU']>=3? "YOU ARE THE WINNER": score['CPU']>=3? "YOU LOST": "IT'S A TIE "
-}
+const choice = document.querySelectorAll('.choices')
+choice.forEach(btn => {
+  btn.addEventListener('click', () => {
+    playRound(btn.innerHTML)
+  })
+})
